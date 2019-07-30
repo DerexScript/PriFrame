633f0e5791a8c347ca786c6418d39f6c1263eaf9
DerexScript <Derex@outlook.com.br>
2019-07-29 23:13:24 -0300

    ...

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<title>priframe - <?php echo $arr["titulo"]; ?></title>
-	<link rel="stylesheet" href="./app/css/style.css">
+	<link rel="stylesheet" href="./public/css/style.css">
 </head>
 <body>
 	
